73dd952f4478e5bcc1d29835212a4638e72d5d0f
mihalay123 <mihalay26@gmail.com>
2025-02-12 20:41:34 +0400

    init next project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,3 @@
-import "eslint-plugin-simple-import-sort";
-
 import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -14,61 +12,13 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     extends: ["next"],
-    plugins: ["simple-import-sort"],
     rules: {
       quotes: ["error", "double"],
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off",
       "no-console": "warn",
+      "import/no-anonymous-default-export": "off",
     },
-    overrides: [
-      {
-        files: ["*.ts", "*.tsx", "*.js", "*.jsx"],
-        rules: {
-          "max-len": [
-            "error",
-            {
-              code: 120,
-              tabWidth: 2,
-              ignoreStrings: true,
-              ignorePattern: "^import\\s.+\\sfrom\\s.+;$",
-            },
-          ],
-          // "@typescript-eslint/ban-types": 1,
-          "simple-import-sort/imports": [
-            "warn",
-            {
-              groups: [
-                // `react` first, `next` second, then packages starting with a character
-                ["^react$"],
-                // other packages
-                ["^[a-z]"],
-                // Packages starting with `@/`,
-                ["^@/"],
-                // Utils from project
-                ["^(@/utils)(/.*|$)", "^(./utils)(/.*|$)"],
-                // Style imports
-                ["^.+\\.s?css$", "^.+\\.s?styled$"],
-                // Assets imports
-                ["^@/assets/"],
-                // Imports starting with `../`
-                ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
-                // Imports starting with `./`
-                ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
-                // Packages starting with `~`
-                ["^~"],
-                // Interface && Type -- last import
-                ["^.*\\u0000$"],
-                ["^\\u0000"],
-                ["^@?\\w"],
-                ["^"],
-                ["^\\."],
-              ],
-            },
-          ],
-        },
-      },
-    ],
   }),
 ];
 
