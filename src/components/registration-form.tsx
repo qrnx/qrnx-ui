bb470480ce0f27487392fae50bc169970d195b04
Egor <152776943+Sulaev@users.noreply.github.com>
2025-02-13 13:46:55 +0300

    QR-26: Add sign up page (#3)
    * add signUp page, add registration-form.tsx
    
    * rename path folder, edit sign up discription
    
    * remove google sign in button

--- src/components/registration-form.tsx
+++ src/components/registration-form.tsx
@@ -11,7 +11,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import Link from "next/link"
 
-export function LoginForm({
+export function RegistrationForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"div">) {
@@ -19,9 +19,9 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
-          <CardTitle className="text-2xl">Sign In</CardTitle>
+          <CardTitle className="text-2xl">Sign Up</CardTitle>
           <CardDescription>
-            Enter your email below to login to your account
+            Enter your email and password below to registration your account
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -39,26 +39,17 @@ export function LoginForm({
               <div className="grid gap-2">
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
-                  <Link
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </Link>
                 </div>
                 <Input id="password" type="password" required />
               </div>
               <Button type="submit" className="w-full">
-                Login
+                Create account
               </Button>
-              {/* <Button variant="outline" className="w-full">
-                Login with Google
-              </Button> */}
             </div>
             <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
+              Already have an account?{" "}
               <Link href="#" className="underline underline-offset-4">
-                Sign up
+                Sign in
               </Link>
             </div>
           </form>
