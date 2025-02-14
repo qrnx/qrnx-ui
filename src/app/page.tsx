bb1936acaf0be43d46bc7a7f32c41d33771e8751
mihalay123 <mihalay26@gmail.com>
2025-02-14 14:40:19 +0400

    add i18n

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import { ThemeToggle } from "@/components/theme-toggle";
+"use client";
+
+import { ThemeToggle } from "@/components/ui/theme-toggle";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
+import { useTranslation } from "react-i18next";
+import { LanguageToggle } from "@/components/ui/language-toggle";
 
 export default function Home() {
+  const { t } = useTranslation();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -15,6 +21,10 @@ export default function Home() {
           height={38}
           priority
         />
+
+        <LanguageToggle />
+
+        {t("hello")}
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
