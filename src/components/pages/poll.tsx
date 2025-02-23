8019e848c4a84b4033ffe73024f32ba88a028498
Mihail <mihalay26@gmail.com>
2025-02-23 15:50:29 +0400

    add Poll layout (#23)

--- src/components/pages/poll.tsx
+++ src/components/pages/poll.tsx
@@ -6,6 +6,7 @@ import { useParams, usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { AnswerOption } from "@/types/answerOptions";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export default function Poll() {
   const { pollId } = useParams();
@@ -33,14 +34,27 @@ export default function Poll() {
     return <Link href={`${pathname}/${documentId}`}>{text}</Link>;
   };
 
-  return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <h1>{title} Page</h1>
+  const cellCommonClasses =
+    "bg-primary/10 w-full col-span-1 aspect-square sm:aspect-2/1 lg:aspect-auto max-h-[300px] ";
 
+  return (
+    <div className="flex flex-col items-center justify-start w-full h-full max-h-full py-8 gap-2 font-[family-name:var(--font-geist-sans)]">
+      <h1>{title} Page</h1>
       <div>
         {answerOptions.map((answerOption) => (
           <div key={answerOption.id}>{renderPollAnswerLink(answerOption)}</div>
         ))}
+      </div>
+      <div className="grid max-h-[900px] min-h-[700px] h-full w-full grid-rows-[repeat(autofit,minmax(150,1fr))] grid-cols-1 lg:grid-cols-3 gap-3">
+        <div className={cellCommonClasses}></div>
+        <div className={cn(cellCommonClasses, "lg:col-span-2")}></div>
+
+        <div className={cn(cellCommonClasses, "lg:col-span-2")}></div>
+        <div className={cellCommonClasses}></div>
+
+        <div className={cellCommonClasses}></div>
+        <div className={cellCommonClasses}></div>
+        <div className={cellCommonClasses}></div>
       </div>
     </div>
   );
