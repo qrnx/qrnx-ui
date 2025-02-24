f855b0cdddb563189a1b3859cdf26be572d42bf5
Sulaev <sulaev2004@mail.ru>
2025-02-24 13:41:03 +0300

    add russian translate

--- src/components/ui/InformationCard.tsx
+++ src/components/ui/InformationCard.tsx
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import {
   Card,
   CardContent,
@@ -8,12 +9,11 @@ import {
 import { Users, Percent, Calendar } from "lucide-react";
 
 export const InformationCard = () => {
+  const t = useTranslations("informationCard");
   return (
     <Card className="flex flex-col justify-between h-full">
       <CardHeader className="flex gap-3">
-        <CardTitle className="text-2xl font-semibold">
-          General Information
-        </CardTitle>
+        <CardTitle className="text-2xl font-semibold">{t("general")}</CardTitle>
         <CardDescription className="text-sm font-medium text-primary">
           The "Tastes of the World" restaurant offers delicious cuisine and a
           warm atmosphere
@@ -22,15 +22,15 @@ export const InformationCard = () => {
       <CardContent className="flex flex-col gap-1.5">
         <div className="flex gap-2">
           <Users />
-          <div>Total: 503</div>
+          <div>{t("cardContent.total")}: 503</div>
         </div>
         <div className="flex gap-2">
           <Percent />
-          <div>Positive responses: 0.54</div>
+          <div>{t("cardContent.responses")}: 0.54</div>
         </div>
         <div className="flex gap-2">
           <Calendar />
-          <div>Date of creation: 12.04.2025</div>
+          <div>{t("cardContent.date")}: 12.04.2025</div>
         </div>
       </CardContent>
     </Card>
