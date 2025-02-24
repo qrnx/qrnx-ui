3a9acee9bf8734839035e0a158049b1371bccca3
Egor <152776943+Sulaev@users.noreply.github.com>
2025-02-24 19:45:10 +0300

    QR-66: Add mainchartcard component (#29)
    * update package.json
    
    * add time-switcher, add main-chart-card, add toggle-group
    
    * add import main-chard-card on poll-page
    
    * add timeIntervals type
    
    * add russian translations
    
    * add timeInterval logic
    
    * add style on time-swircher
    
    * add trendingIcon, add trendingBlock
    
    * fix card
    
    * add chart component on mainChartCard

--- src/components/ui/Information-card.tsx
+++ src/components/ui/Information-card.tsx
@@ -1,25 +1,19 @@
 import { useTranslations } from "next-intl";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "./card";
+import { Card, CardDescription, CardTitle } from "./card";
 import { Users, Percent, Calendar } from "lucide-react";
 
 export const InformationCard = () => {
   const t = useTranslations("informationCard");
   return (
-    <Card className="flex flex-col justify-between h-full">
-      <CardHeader className="flex gap-3">
+    <Card className="flex flex-col justify-between gap-8 h-full p-4">
+      <div className="flex flex-col gap-3">
         <CardTitle className="text-2xl font-semibold">{t("general")}</CardTitle>
         <CardDescription className="text-sm font-medium text-primary">
           The "Tastes of the World" restaurant offers delicious cuisine and a
           warm atmosphere
         </CardDescription>
-      </CardHeader>
-      <CardContent className="flex flex-col gap-1.5">
+      </div>
+      <div className="flex flex-col gap-1.5">
         <div className="flex gap-2">
           <Users />
           <div>{t("total")}: 503</div>
@@ -32,7 +26,7 @@ export const InformationCard = () => {
           <Calendar />
           <div>{t("date")}: 12.04.2025</div>
         </div>
-      </CardContent>
+      </div>
     </Card>
   );
 };
---
+++ src/components/ui/time-switcher.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Card } from "./card";
+import { TimeIntervals } from "@/types/timeIntervals";
+import { useTranslations } from "next-intl";
+
+const TIME_INTERVALS = [
+  TimeIntervals.WEEK,
+  TimeIntervals.MONTH,
+  TimeIntervals.YEAR,
+  TimeIntervals.ALL,
+];
+
+interface TimeSwitcherProps {
+  initialInterval: TimeIntervals;
+  onChange: (value: TimeIntervals) => void;
+}
+
+export const TimeSwitcher = ({
+  initialInterval,
+  onChange,
+}: TimeSwitcherProps) => {
+  const [timeInterval, setTimeInterval] = useState(initialInterval);
+  const t = useTranslations("poll.timeIntervals");
+  console.log(timeInterval);
+
+  return (
+    <Card className="rounded-lg">
+      <ToggleGroup
+        type="single"
+        value={timeInterval}
+        onValueChange={(value) => {
+          if (!TIME_INTERVALS.includes(value as TimeIntervals)) {
+            return;
+          }
+          setTimeInterval(value as TimeIntervals);
+          onChange(value as TimeIntervals);
+        }}
+        className="flex justify-between p-[4] gap-0.5 rounded-lg shadow-sm"
+      >
+        {TIME_INTERVALS.map((item) => {
+          return (
+            <ToggleGroupItem
+              value={item}
+              key={item}
+              className={`text-xs font-semibold px-4 py-2 rounded-md ${
+                timeInterval === item ? "bg-accent text-primary" : ""
+              }`}
+            >
+              {t(item)}
+            </ToggleGroupItem>
+          );
+        })}
+      </ToggleGroup>
+    </Card>
+  );
+};
