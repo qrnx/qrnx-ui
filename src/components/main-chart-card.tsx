5b0509100795606312902db45eef62cd46e0e143
Sulaev <sulaev2004@mail.ru>
2025-02-24 16:49:55 +0300

    add trendingIcon, add trendingBlock

--- src/components/main-chart-card.tsx
+++ src/components/main-chart-card.tsx
@@ -2,6 +2,7 @@ import { Card } from "./ui/card";
 import { TimeSwitcher } from "./ui/time-switcher";
 import { useState } from "react";
 import { TimeIntervals } from "@/types/timeIntervals";
+import { TrendingDown, TrendingUp } from "lucide-react";
 
 export const MainChartCard = () => {
   const [timeInterval, setTimeInterval] = useState(TimeIntervals.WEEK);
@@ -9,7 +10,7 @@ export const MainChartCard = () => {
 
   return (
     <Card className="flex h-full flex-col justify-between p-[20]">
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-5">
         <div className="flex justify-between">
           <div className="text-2xl font-semibold">Main Chart</div>
           <TimeSwitcher
@@ -17,7 +18,16 @@ export const MainChartCard = () => {
             onChange={setTimeInterval}
           />
         </div>
-        <div>ЧИСЛА И СТРЕЛОЧКИ</div>
+        <div className="flex gap-6 text-2xl font-medium">
+          <div className="flex gap-2">
+            324
+            <TrendingUp className="size-8 text-(--chart-2)" />
+          </div>
+          <div className="flex gap-2">
+            54
+            <TrendingDown className="size-8 text-(--chart-1)" />
+          </div>
+        </div>
       </div>
       <div>График {timeInterval}</div>
     </Card>
