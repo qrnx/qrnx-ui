d628cf41e53aa792bf5871bd9ebfa7da8e58ff3a
Egor <152776943+Sulaev@users.noreply.github.com>
2025-02-27 17:26:50 +0300

    QR-74: Add footer (#38)
    * add fotter.tsx, add copyright icon
    
    * add russian translate
    
    * fix footer.tsx
    
    * fix russian translate
    
    * add mailto link

---
+++ src/components/footer.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import styles from "@/components/layout.module.css";
+import { ROUTES } from "@/config/routes";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Discord from "@/assets/Discord.svg";
+import YouTube from "@/assets/YouTube.svg";
+import Instagram from "@/assets/Instagram.svg";
+import { useTranslations } from "next-intl";
+
+const routesWIthoutHeader = [ROUTES.signIn, ROUTES.signUp];
+
+export const Footer = () => {
+  const t = useTranslations("footer");
+  const pathname = usePathname();
+  if (routesWIthoutHeader.includes(pathname)) {
+    return null;
+  }
+  return (
+    <footer
+      className={cn(
+        styles["border-grid"],
+        "sticky top-0 z-50 w-full border-t bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+      )}
+    >
+      <div className={styles["container-wrapper"]}>
+        <div
+          className={cn(
+            styles["container"],
+            "flex h-10 md:h-17 items-center justify-between"
+          )}
+        >
+          <div className="text-sm opacity-50">Copyright © 2025 QRpools.com</div>
+          <div className="flex gap-8 text-sm opacity-50">
+            <Link href={""}>{t("privacy")}</Link>
+            <Link href={""}>{t("terms")}</Link>
+            <Link href={"mailto:mihalay26@gmail.com?subject=Support%20letter"}>
+              {t("support")}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
