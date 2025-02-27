f61676d6eb8e10513b676096dbce159f6c86b7a3
Egor <152776943+Sulaev@users.noreply.github.com>
2025-02-27 15:49:58 +0300

    QR-73: Add server data to Information card (#37)
    * add server data to InformationCard
    
    * fix information-card
    
    * fix InformationCard

--- src/components/Information-card.tsx
+++ src/components/Information-card.tsx
@@ -1,19 +1,33 @@
 import { useTranslations } from "next-intl";
 import { Card, CardDescription, CardTitle } from "./ui/card";
 import { Users, Percent, Calendar } from "lucide-react";
+import { Poll } from "@/types/poll";
 
-export const InformationCard = () => {
+interface InformationCardProps {
+  poll: Poll;
+}
+
+export const InformationCard = ({ poll }: InformationCardProps) => {
   const t = useTranslations("informationCard");
+  const { description, createdAt } = poll;
+  const date = new Date(createdAt);
+  const formattedDate = new Intl.DateTimeFormat("ru-RU", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  }).format(date);
+
   return (
     <Card className="flex flex-col justify-between gap-8 h-full p-4">
       <div className="flex flex-col gap-3">
         <CardTitle className="text-xl sm:text-2xl font-semibold">
           {t("general")}
         </CardTitle>
-        <CardDescription className="text-sm font-medium text-primary">
-          The "Tastes of the World" restaurant offers delicious cuisine and a
-          warm atmosphere
-        </CardDescription>
+        {description && (
+          <CardDescription className="text-sm font-medium text-primary">
+            {description}
+          </CardDescription>
+        )}
       </div>
       <div className="flex flex-col gap-1.5">
         <div className="flex gap-2">
@@ -26,7 +40,9 @@ export const InformationCard = () => {
         </div>
         <div className="flex gap-2">
           <Calendar />
-          <div>{t("date")}: 12.04.2025</div>
+          <div>
+            {t("date")}: {formattedDate}
+          </div>
         </div>
       </div>
     </Card>
