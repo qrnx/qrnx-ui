ca4015462acb4552cffb64cc77c8ffaa569dae53
Sulaev <sulaev2004@mail.ru>
2025-03-03 15:59:03 +0300

    add not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound: React.FC = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>404 - Страница не найдена</h1>
+      <p>Извините, запрошенная страница не существует.</p>
+    </div>
+  );
+};
+
+export default NotFound;
