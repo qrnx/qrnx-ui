97f0099fee6b39dda618b6bee411321e429a1414
Sulaev <sulaev2004@mail.ru>
2025-03-03 16:21:39 +0300

    add style on 404page, add russian translate

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,27 @@
+import { ROUTES } from "@/config/routes";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
 import React from "react";
+import { SquareArrowOutUpRight } from "lucide-react";
 
 const NotFound: React.FC = () => {
+  const t = useTranslations("404page");
+
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>404 - Страница не найдена</h1>
-      <p>Извините, запрошенная страница не существует.</p>
+    <div className="flex flex-col items-center justify-center h-screen gap-8">
+      <div className="flex items-center text-9xl">404</div>
+      <div className="flex flex-col gap-2">
+        <div> {t("massage")}</div>
+        <div>
+          <Link
+            className="flex items-center gap-2 underline"
+            href={ROUTES.home}
+          >
+            {t("homeLink")}
+            <SquareArrowOutUpRight size={20} />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
