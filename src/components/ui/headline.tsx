48662c47c634c62e91de9219041b703dfa3ef03d
mihalay123 <mihalay26@gmail.com>
2025-03-11 16:37:26 +0400

    add back button

--- src/components/ui/headline.tsx
+++ src/components/ui/headline.tsx
@@ -1,3 +1,7 @@
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+
 interface HeadlineProps {
   title: string;
   buttonsContainer: React.ReactNode;
@@ -7,9 +11,18 @@ export const Headline: React.FC<HeadlineProps> = ({
   title,
   buttonsContainer,
 }) => {
+  const { pollId } = useParams();
+
   return (
     <div className="flex items-center justify-between w-full">
-      <div className="text-xl sm:text-4xl font-semibold">{title}</div>
+      <div className="flex items-center gap-1 sm:gap-2">
+        {Boolean(pollId) ? (
+          <Link href={"/dashboard"}>
+            <ChevronLeft size={30} className="hover:opacity-70" />
+          </Link>
+        ) : null}
+        <div className="text-xl sm:text-4xl font-semibold">{title}</div>
+      </div>
       <div className="flex items-center gap-2 sm:gap-3">{buttonsContainer}</div>
     </div>
   );
